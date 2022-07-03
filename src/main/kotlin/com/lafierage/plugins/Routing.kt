caf91a70845dd233c8a6f1f10afc17fddb752f5f
Lafie-rage <corentin.destrez62@gmail.com>
2022-07-03 16:24:39 +0200

    Adding POST /courses route

--- src/main/kotlin/com/lafierage/plugins/Routing.kt
+++ src/main/kotlin/com/lafierage/plugins/Routing.kt
@@ -4,8 +4,11 @@ import com.lafierage.data.dao.CourseDao
 import com.lafierage.data.dao.CourseDaoImpl
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
 import kotlinx.coroutines.runBlocking
 
 fun Application.configureRouting() {
@@ -60,6 +63,30 @@ fun Application.configureRouting() {
             }
             // endregion
 
+            // region POST routes
+            post {
+                val formParameters = call.receiveParameters()
+                try {
+                    val createdCourse = courseDao.addNewCourse(
+                        formParameters.getOrFail("title"),
+                        formParameters.getOrFail("level").toInt(),
+                        formParameters.getOrFail("isActive").toBooleanStrict(),
+                        formParameters.getOrFail("rank").toInt(),
+                    )
+                    createdCourse?.let {
+                        call.respond(createdCourse)
+                    } ?: call.respondText(
+                        "Error while creating item",
+                        status = HttpStatusCode.InternalServerError,
+                    )
+                } catch (e: MissingRequestParameterException) {
+                    call.respondText(
+                        "Missing parameter : ${e.parameterName}",
+                        status = HttpStatusCode.BadRequest,
+                    )
+                }
+            }
+            // endregion
         }
     }
     routing {
